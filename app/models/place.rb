dc0e300b6445aba728db580da489dfa4ba577602
Maurice Matthews <mauriceematthews@gmail.com>
2014-07-17 02:09:53 +0000

    Add map info

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,4 +1,7 @@
 class Place < ActiveRecord::Base
 	belongs_to :user
-	validates :name, :presence => true
+ 	geocoded_by :address 
+ 	after_validation :geocode
+
+	validates :address, :presence => true, :length => { :minimum => 3}
 end
