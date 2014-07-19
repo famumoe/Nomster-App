b6de3508749a144bc7a54ef284f66e7de10a6108
Maurice Matthews <mauriceematthews@gmail.com>
2014-07-19 16:34:15 +0000

    Created database for comments

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,7 +1,9 @@
 class Place < ActiveRecord::Base
 	belongs_to :user
+	has_many :comments
+
  	geocoded_by :address 
  	after_validation :geocode
 
-	 validates :name, :presence => true
+	validates :name, :presence => true
 end
